c15a0be76bab856f238c230e531051bad643f379
Huan Nguyen <huancal1984@gmail.com>
2019-12-20 23:28:00 -0800

    home.html and fiends.js updated

--- app/data/friend.js
+++ app/data/friend.js
@@ -1,19 +1,36 @@
-var friends = [{
-    "name": "Captain America",
-    "photo": "https://cnet1.cbsistatic.com/img/KUgii2GnrjOeEK4mpfsZLecvqkY=/756x425/2017/08/03/75c3b0ae-5a2d-4d75-b72b-055247b4378f/marvelinfinitywar-captainamerica.jpg",
-    "scores": [
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-    ]
-}]
+var friends = [
+
+    {
+        "name": "Captain America",
+        "photo": "https://vignette.wikia.nocookie.net/p__/images/d/d7/CapAmerica-EndgameProfile.jpg/revision/latest/scale-to-width-down/699?cb=20190425114854&path-prefix=protagonist",
+        "scores": [5, 5, 1, 2, 3, 1, 2, 5, 1, 1, ]
+
+    },
+
+    {
+        "name": "Iron Man",
+        "photo": "https://vignette.wikia.nocookie.net/p__/images/3/35/IronMan-EndgameProfile.jpg/revision/latest/scale-to-width-down/699?cb=20190425080838&path-prefix=protagonist",
+        "scores": [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+    },
+
+    {
+        "name": "Thor",
+        "photo": "https://vignette.wikia.nocookie.net/p__/images/1/13/Thor-EndgameProfile.jpg/revision/latest/scale-to-width-down/699?cb=20190425114542&path-prefix=protagonist",
+        "scores": [5, 3, 1, 3, 5, 2, 3, 2, 2, 5, ]
+    },
+
+    {
+        "name": "The Hulk",
+        "photo": "https://vignette.wikia.nocookie.net/p__/images/1/1e/Hulk_endgame.png/revision/latest/scale-to-width-down/693?cb=20190509144938&path-prefix=protagonist",
+        "scores": [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+    },
+    {
+        "name": "Spider Man ",
+        "photo": "https://vignette.wikia.nocookie.net/p__/images/d/df/Peter_Parker_FFH_Textless_Poster.jpg/revision/latest/scale-to-width-down/700?cb=20190707173453&path-prefix=protagonist",
+        "scores": [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+    },
+
+];
 
 
 
