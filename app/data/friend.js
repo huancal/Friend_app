a9bf2de4032cd1583e267d36d7920141bab14cfd
Huan Nguyen <huancal1984@gmail.com>
2019-12-20 21:03:06 -0800

    apiroutes updated with for loop

--- app/data/friend.js
+++ app/data/friend.js
@@ -1,9 +1,4 @@
-$(document).ready(() => {
-
-
-})
-
-var friends = {
+var friends = [{
     "name": "Captain America",
     "photo": "https://cnet1.cbsistatic.com/img/KUgii2GnrjOeEK4mpfsZLecvqkY=/756x425/2017/08/03/75c3b0ae-5a2d-4d75-b72b-055247b4378f/marvelinfinitywar-captainamerica.jpg",
     "scores": [
@@ -18,4 +13,10 @@ var friends = {
         4,
         1
     ]
-}
+}]
+
+
+
+
+
+module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,49 @@
 const express = require('express');
 const router = express.Router();
 
+let friends = require('../data/friend');
+
 router.get('/', (req, res) => {
-    res.send('testing api route')
+    res.send(friends);
 });
 
+router.post('/', (req, res) => {
+    // new submit
 
+    var newSubmission = req.body;
+
+    var bestMatch = {};
+
+    var minDifference = 100;
+
+
+
+    for (var i = 0; i < friends.length; i++) {
+
+        var totalDifference = 0;
+
+        var currentFriend = friends[i];
+
+
+        for (var j = 0; j < currentFriend.scores.length; i++) {
+            totalDifference += Math.abs(parseInt(newSubmission.scores[j]) - parseInt(currentFriend.scores[j]));
+
+        };
+
+        if (totalDifference <= minDifference) {
+            bestMatch = currentFriend;
+            minDifference = totalDifference;
+        };
+    }
+
+    friends.push(newSubmission);
+
+    res.json(bestMatch);
+
+
+    // 
+
+});
 
 
 module.exports = router;
